faa189aec2f3d72f766e9787af0c80da059ac60d
Leonardo Andrade <carlos.andrade@coppel.com>
2023-06-08 19:24:12 -0600

    Se vio el tema de generadores. Se realizaron challenge sobre este tema

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -24,3 +24,21 @@ const specificData = async (urlAPI) => {
 };
 
 specificData(API);
+
+// # DESAFIO
+/*
+ * En este desafío vas a conectarte a una API que no existe, por ende debes capturar el error 
+ * haciendo uso de try/catch y lanzar un error con el mensaje API Not Found.
+ * Para lanzar el error debes usar el siguiente bloque de código:
+ ! throw new Error('API Not Found');
+*/
+async function runCode() {
+  const url = 'https://domain-api-com';
+  try {
+    return await fetch(url);
+  } catch {
+    throw new Error("API Not Found");
+  }
+}
+
+await runCode();
