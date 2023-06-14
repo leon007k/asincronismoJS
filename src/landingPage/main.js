053852a4b78d832eda998b00cf23e8d83d6c59ea
Leonardo Andrade <carlos.andrade@coppel.com>
2023-06-13 19:40:19 -0600

    se corrige archivo

--- src/landingPage/main.js
+++ src/landingPage/main.js
@@ -7,14 +7,6 @@ const options = {
   }
 };
 
-/* try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
-} */
-
 const content = null || document.getElementById('content');
 
 async function fetchData(urlAPI) {
